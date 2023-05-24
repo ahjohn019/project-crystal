a2c0a69ae43828e6ace6c5abf64c27ab9843621c
ahjohn019 <yewrui@hotmail.com>
2023-05-24 19:54:56 +0800

    init commit - two

--- vuejs/admin-app/modules/dashboard/views/MainPage.vue
+++ vuejs/admin-app/modules/dashboard/views/MainPage.vue
@@ -2,5 +2,22 @@
     <div>
         <NavBar />
         <h1>This is dashboard</h1>
+        <el-row class="mb-4">
+          <el-button>Default</el-button>
+          <el-button type="primary">Primary</el-button>
+          <el-button type="success">Success</el-button>
+          <el-button type="info">Info</el-button>
+          <el-button type="warning">Warning</el-button>
+          <el-button type="danger">Danger</el-button>
+        </el-row>
+
+        <el-row class="mb-4">
+          <el-button plain>Plain</el-button>
+          <el-button type="primary" plain>Primary</el-button>
+          <el-button type="success" plain>Success</el-button>
+          <el-button type="info" plain>Info</el-button>
+          <el-button type="warning" plain>Warning</el-button>
+          <el-button type="danger" plain>Danger</el-button>
+        </el-row>
     </div>
 </template>
