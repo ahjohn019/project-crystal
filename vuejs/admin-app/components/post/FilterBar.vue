8cf1c0267c9b47bca9e5aced1fa81a98d13e69d3
ahjohn019 <yewrui@hotmail.com>
2023-12-17 13:45:00 +0800

    02-17122023
    - Fix filter bar search ui responsive issues

--- vuejs/admin-app/components/post/FilterBar.vue
+++ vuejs/admin-app/components/post/FilterBar.vue
@@ -1,92 +1,88 @@
 <template>
     <div class="row p-2 bg-white mt-5">
-        <div class="col col-md-6 col-lg-9 filter-posts-search-bar">
+        <div class="col-12 col-md-6 filter-posts-search-bar">
             <q-input outlined dense label="Search" v-model="postKeywords">
                 <template v-slot:append>
                     <q-icon name="search" @click="handlePostSearch" />
                 </template>
             </q-input>
         </div>
-        <div class="col col-md-2 col-lg-1 text-center">
-            <q-btn-dropdown
-                label="Filters"
-                class="filter-posts-bar"
-                dropdown-icon="expand_more"
-            >
-                <q-list>
-                    <q-item clickable>
-                        <q-item-section>
-                            <q-item-label>Photos</q-item-label>
-                        </q-item-section>
-                    </q-item>
-
-                    <q-item clickable>
-                        <q-item-section>
-                            <q-item-label>Videos</q-item-label>
-                        </q-item-section>
-                    </q-item>
-
-                    <q-item clickable>
-                        <q-item-section>
-                            <q-item-label>Articles</q-item-label>
-                        </q-item-section>
-                    </q-item>
-                </q-list>
-            </q-btn-dropdown>
-        </div>
-        <div class="col col-md-2 col-lg-1 text-center">
-            <q-btn-dropdown
-                class="filter-posts-bar"
-                dropdown-icon="expand_more"
-                label="Category"
-            >
-                <q-list>
-                    <q-item clickable>
-                        <q-item-section>
-                            <q-item-label>Photos</q-item-label>
-                        </q-item-section>
-                    </q-item>
-
-                    <q-item clickable>
-                        <q-item-section>
-                            <q-item-label>Videos</q-item-label>
-                        </q-item-section>
-                    </q-item>
-
-                    <q-item clickable>
-                        <q-item-section>
-                            <q-item-label>Articles</q-item-label>
-                        </q-item-section>
-                    </q-item>
-                </q-list>
-            </q-btn-dropdown>
-        </div>
-        <div class="col col-md-2 col-lg-1 text-center">
-            <q-btn-dropdown
-                class="filter-posts-bar"
-                dropdown-icon="expand_more"
-                label="Latest"
-            >
-                <q-list>
-                    <q-item clickable>
-                        <q-item-section>
-                            <q-item-label>Photos</q-item-label>
-                        </q-item-section>
-                    </q-item>
-
-                    <q-item clickable>
-                        <q-item-section>
-                            <q-item-label>Videos</q-item-label>
-                        </q-item-section>
-                    </q-item>
-
-                    <q-item clickable>
-                        <q-item-section>
-                            <q-item-label>Articles</q-item-label>
-                        </q-item-section>
-                    </q-item>
-                </q-list>
-            </q-btn-dropdown>
+        <div class="col-12 col-md-6 row">
+            <div class="col text-center">
+                <q-btn-dropdown
+                    label="Filters"
+                    class="filter-posts-bar"
+                    dropdown-icon="expand_more"
+                >
+                    <q-list>
+                        <q-item clickable>
+                            <q-item-section>
+                                <q-item-label>Photos</q-item-label>
+                            </q-item-section>
+                        </q-item>
+                        <q-item clickable>
+                            <q-item-section>
+                                <q-item-label>Videos</q-item-label>
+                            </q-item-section>
+                        </q-item>
+                        <q-item clickable>
+                            <q-item-section>
+                                <q-item-label>Articles</q-item-label>
+                            </q-item-section>
+                        </q-item>
+                    </q-list>
+                </q-btn-dropdown>
+            </div>
+            <div class="col text-center">
+                <q-btn-dropdown
+                    class="filter-posts-bar"
+                    dropdown-icon="expand_more"
+                    label="Category"
+                >
+                    <q-list>
+                        <q-item clickable>
+                            <q-item-section>
+                                <q-item-label>Photos</q-item-label>
+                            </q-item-section>
+                        </q-item>
+                        <q-item clickable>
+                            <q-item-section>
+                                <q-item-label>Videos</q-item-label>
+                            </q-item-section>
+                        </q-item>
+                        <q-item clickable>
+                            <q-item-section>
+                                <q-item-label>Articles</q-item-label>
+                            </q-item-section>
+                        </q-item>
+                    </q-list>
+                </q-btn-dropdown>
+            </div>
+            <div class="col text-center">
+                <q-btn-dropdown
+                    class="filter-posts-bar"
+                    dropdown-icon="expand_more"
+                    label="Latest"
+                >
+                    <q-list>
+                        <q-item clickable>
+                            <q-item-section>
+                                <q-item-label>Photos</q-item-label>
+                            </q-item-section>
+                        </q-item>
+                        <q-item clickable>
+                            <q-item-section>
+                                <q-item-label>Videos</q-item-label>
+                            </q-item-section>
+                        </q-item>
+                        <q-item clickable>
+                            <q-item-section>
+                                <q-item-label>Articles</q-item-label>
+                            </q-item-section>
+                        </q-item>
+                    </q-list>
+                </q-btn-dropdown>
+            </div>
         </div>
     </div>
 </template>
